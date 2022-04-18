d221b78970fe38bbc4a8807db0605e4b42f97ef3
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-04-18 11:28:23 +0700

    Set dynamic inline styles

--- 6 - Styling React Component/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 6 - Styling React Component/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -3,23 +3,32 @@ import React, { useState } from 'react';
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
-const CourseInput = props => {
+const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
+    if (enteredValue.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} style={{ backgroundColor: !isValid ? 'salmon' : 'transparent', borderColor: !isValid ? 'red' : '#ccc' }} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
