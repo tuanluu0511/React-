b675a47a408439b70a8deb59090245e18db9f9dd
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-04-20 22:03:59 +0700

    Use the useEffect Cleanup function

--- 10 - Handling Side Effects, Using Reducers and Context API/src/components/Login/Login.js
+++ 10 - Handling Side Effects, Using Reducers and Context API/src/components/Login/Login.js
@@ -12,7 +12,15 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(enteredEmail.includes('@') && enteredPassword.trim().length > 6);
+    const identifier = setTimeout(() => {
+      console.log('Checking form validity!');
+      setFormIsValid(enteredEmail.includes('@') && enteredPassword.trim().length > 6);
+    }, 500);
+
+    return () => {
+      console.log('Clean up');
+      clearTimeout(identifier);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
