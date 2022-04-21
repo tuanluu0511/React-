d279757bcf48e6d9428b627626db15df3e6e184b
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-04-21 22:46:18 +0700

    useEffect sumary

--- 10 - Handling Side Effects, Using Reducers and Context API/src/components/Login/Login.js
+++ 10 - Handling Side Effects, Using Reducers and Context API/src/components/Login/Login.js
@@ -12,6 +12,13 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
+    console.log('EFFECT RUNNING');
+    return () => {
+      console.log('EFFECT CLEANUP');
+    };
+  }, []);
+
+  useEffect(() => {
     const identifier = setTimeout(() => {
       console.log('Checking form validity!');
       setFormIsValid(enteredEmail.includes('@') && enteredPassword.trim().length > 6);
