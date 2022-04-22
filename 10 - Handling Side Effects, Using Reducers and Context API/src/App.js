5160c70e8e2f66a4948dd97cc2cea58174b40f43
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-04-22 22:23:27 +0700

    Context API with Consumer

--- 10 - Handling Side Effects, Using Reducers and Context API/src/App.js
+++ 10 - Handling Side Effects, Using Reducers and Context API/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
+import AuthContext from './store/auth-context';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
@@ -27,13 +28,17 @@ function App() {
   };
 
   return (
-    <React.Fragment>
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+      }}
+    >
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
