0727bc38cc5962a3668ec0232ef13a1195dca9c5
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-04-22 23:13:28 +0700

    Custom Context Provider Component

--- 10 - Handling Side Effects, Using Reducers and Context API/src/store/auth-context.js
+++ 10 - Handling Side Effects, Using Reducers and Context API/src/store/auth-context.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
 });
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storeUserLoggedInInfo = localStorage.getItem('isLoggedIn');
+    if (storeUserLoggedInInfo === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    localStorage.setItem('isLoggedIn', '1');
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+    localStorage.removeItem('isLoggedIn');
+  };
+
+  return <AuthContext.Provider value={{ isLoggedIn: isLoggedIn, onLogout: logoutHandler, onLogin: loginHandler }}>{props.children}</AuthContext.Provider>;
+};
 
 export default AuthContext;
