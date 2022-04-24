c66eee8a808f8c8a6fb08674da3c525e40f9c282
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-04-24 11:45:17 +0700

    Add Header components

---
+++ 11 - Food Order App/src/App.js
@@ -0,0 +1,12 @@
+import { Fragment } from 'react/cjs/react.production.min';
+import Header from './components/Layout/Header';
+
+function App() {
+  return (
+    <Fragment>
+      <Header></Header>
+    </Fragment>
+  );
+}
+
+export default App;
