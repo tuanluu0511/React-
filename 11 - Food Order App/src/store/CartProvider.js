565293ec63c321c46c2ec223d804a8038260ed72
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-05-04 14:41:13 +0700

    Add Cart Reducer

--- 11 - Food Order App/src/store/CartProvider.js
+++ 11 - Food Order App/src/store/CartProvider.js
@@ -1,13 +1,35 @@
+import { useReducer } from 'react';
+
 import CartContext from './cart-context';
 
-const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
 
-  const removeItemFromCartHandler = (id) => {};
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD') {
+    const updatedItems = state.items.concat(action.item);
+    const updatedAmount = state.totalAmount + action.item.amount * action.item.price;
+    return { items: updatedItems, totalAmount: updatedAmount };
+  }
+  return defaultCartState;
+};
+
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: 'ADD', item: item });
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: 'REMOVE', id: id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
