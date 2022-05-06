9f037e16f5ff742bc4a9de013633efedfdadd017
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-05-06 08:51:37 +0700

    How React update component

--- 12 - A Look Behind The Scence/src/App.js
+++ 12 - A Look Behind The Scence/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from './components/UI/Button/Button';
 
 import './App.css';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+  const toggleTextHandler = () => {
+    setShowParagraph((prevShowParagraph) => !showParagraph);
+  };
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is Toggle Text</p>}
+      <Button onClick={toggleTextHandler}>Toggle Paragraph</Button>
     </div>
   );
 }
