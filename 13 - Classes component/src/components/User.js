896c385f3d1b797e2c28875d7ab84e78e4470b7b
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-05-06 22:56:28 +0700

    Lifecycle method

--- 13 - Classes component/src/components/User.js
+++ 13 - Classes component/src/components/User.js
@@ -3,6 +3,9 @@ import { Component } from 'react';
 import classes from './User.module.css';
 
 class User extends Component {
+  componentWillUnmount() {
+    console.log('Component Unmounted');
+  }
   render() {
     return <li className={classes.user}>{this.props.name}</li>;
   }
