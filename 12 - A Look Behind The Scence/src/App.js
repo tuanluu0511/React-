b14e1ecda187ec3cb1a4ef0e907c6f6285243e42
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-05-06 11:30:54 +0700

    use Callback and dependencies

--- 12 - A Look Behind The Scence/src/App.js
+++ 12 - A Look Behind The Scence/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import Button from './components/UI/Button/Button';
 import DemoOutput from './components/DemoOutput/DemoOutput';
 
@@ -6,13 +6,24 @@ import './App.css';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
-  const toggleTextHandler = () => {
-    setShowParagraph((prevShowParagraph) => !showParagraph);
+  const [allowToggle, setAllowToggle] = useState(false);
+  console.log('APP RUNNING');
+
+  const toggleTextHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
   };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false}></DemoOutput>
+      <DemoOutput show={showParagraph}></DemoOutput>
+      <Button onClick={allowToggleHandler}> Allow Toggle </Button>
       <Button onClick={toggleTextHandler}>Toggle Paragraph</Button>
     </div>
   );
