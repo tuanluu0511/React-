34dca9c68dae1466662db80128f4167506661c7f
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-05-08 14:31:17 +0700

    Adjust the custom hook logic

--- 15-1/src/App.js
+++ 15-1/src/App.js
@@ -7,21 +7,21 @@ import useHttp from './hooks/use-http';
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const transformTasks = (taskObj) => {
-    const loadedTasks = [];
-
-    for (const taskKey in taskObj) {
-      loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
-    }
-
-    setTasks(loadedTasks);
-  };
-
-  const { isLoading, error, sendRequest: fetchTask } = useHttp({ url: 'https://react-http-d638f-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json' }, transformTasks);
+  const { isLoading, error, sendRequest: fetchTask } = useHttp();
 
   useEffect(() => {
-    fetchTask();
-  }, []);
+    const transformTasks = (taskObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTask({ url: 'https://react-http-d638f-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json' }, transformTasks);
+  }, [fetchTask]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
--- 15-1/src/hooks/use-http.js
+++ 15-1/src/hooks/use-http.js
@@ -1,12 +1,13 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
-const useHttp = (requestConfig, applyData) => {
+const useHttp = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const sendRequest = async (taskText) => {
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
     setIsLoading(true);
     setError(null);
+    
     try {
       const response = await fetch(requestConfig.url, {
         method: requestConfig.method ? requestConfig.method : 'GET',
@@ -25,7 +26,7 @@ const useHttp = (requestConfig, applyData) => {
       setError(err.message || 'Something went wrong!');
     }
     setIsLoading(false);
-  };
+  }, []);
 
   return {
     isLoading: isLoading,
