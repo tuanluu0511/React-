88793b5acf1aa093eead48b0bcb8c2b6e933ccc2
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-05-09 16:04:30 +0700

    Manage overall form validity

--- 16 - Working with form and user input/src/components/SimpleInput.js
+++ 16 - Working with form and user input/src/components/SimpleInput.js
@@ -5,6 +5,11 @@ const SimpleInput = (props) => {
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
   const enteredNameIsValid = enteredName.trim() !== '';
   const nameInputIsInvalid = enteredNameTouched && !enteredNameIsValid;
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
 
   const nameInputChangeHandler = (e) => {
     setEnteredName(e.target.value);
@@ -37,7 +42,7 @@ const SimpleInput = (props) => {
         {nameInputIsInvalid && <p className="error-text">Name can't be empty.</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
