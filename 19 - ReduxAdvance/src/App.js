8600dc363c367ebef7eb18e70a2325389d516747
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-05-15 23:57:06 +0700

    Use useEffect to connect data to firebase

--- 19 - ReduxAdvance/src/App.js
+++ 19 - ReduxAdvance/src/App.js
@@ -1,8 +1,17 @@
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch('https://react-http-d638f-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', { method: 'PUT', body: JSON.stringify(cart) });
+  }, [cart]);
+
   return (
     <Layout>
       <Cart />
