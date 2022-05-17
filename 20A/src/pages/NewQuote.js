fdc5555f4d504706d80bac44d17484497dfbfded
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-05-17 21:29:11 +0700

    Navigate after submit by useHistory

--- 20A/src/pages/NewQuote.js
+++ 20A/src/pages/NewQuote.js
@@ -1,8 +1,13 @@
+import { useHistory } from 'react-router-dom';
+
 import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuote = () => {
+  const history = useHistory();
   const addNewQuoteHandler = (data) => {
     console.log(data);
+
+    history.push('/quotes');
   };
 
   return <QuoteForm onAddQuote={addNewQuoteHandler}></QuoteForm>;
