e534577093859006cf7532005797db55ecc720b5
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-05-17 13:36:25 +0700

    Build page route

--- 20A/src/App.js
+++ 20A/src/App.js
@@ -1,8 +1,21 @@
+import { Route, Switch } from 'react-router-dom';
+import AllQuotes from './components/pages/AllQuotes';
+import NewQuote from './components/pages/NewQuote';
+import QuoteDetail from './components/pages/QuoteDetail';
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Switch>
+      <Route path="/quotes" exact>
+        <AllQuotes />
+      </Route>
+      <Route path="/quotes/:quotedId">
+        <QuoteDetail />
+      </Route>
+      <Route path="/new-quote">
+        <NewQuote />
+      </Route>
+    </Switch>
   );
 }
 
