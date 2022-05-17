b5a02363307101c4025ea3d9c6774c04580de5bc
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-05-17 14:14:10 +0700

    Add comment component to quote detail

--- 20A/src/App.js
+++ 20A/src/App.js
@@ -1,15 +1,18 @@
-import { Route, Switch } from 'react-router-dom';
-import AllQuotes from './components/pages/AllQuotes';
-import NewQuote from './components/pages/NewQuote';
-import QuoteDetail from './components/pages/QuoteDetail';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import AllQuotes from './pages/AllQuotes';
+import NewQuote from './pages/NewQuote';
+import QuoteDetail from './pages/QuoteDetail';
 
 function App() {
   return (
     <Switch>
+      <Route path="/" exact>
+        <Redirect to="/quotes" />
+      </Route>
       <Route path="/quotes" exact>
         <AllQuotes />
       </Route>
-      <Route path="/quotes/:quotedId">
+      <Route path="/quotes/:quoteId">
         <QuoteDetail />
       </Route>
       <Route path="/new-quote">
