4dca092e8997b10650d5f0c3785fae0037d9ccd3
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-05-21 16:16:38 +0700

    Nested route changes in v6

--- 20B/base-example-v5/src/App.js
+++ 20B/base-example-v5/src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
@@ -11,7 +11,8 @@ function App() {
       <MainHeader />
       <main>
         <Routes>
-          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/" element={<Navigate to="/welcome" />}></Route>
+          <Route path="/welcome/*" element={<Welcome />} />
           <Route path="/products" element={<Products />} />
           <Route path="/products/:productId" element={<ProductDetail />} />
         </Routes>
