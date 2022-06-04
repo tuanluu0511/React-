4137d57c9a0064338b4f2de9e31ffe56665639c1
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-06-04 14:11:48 +0700

    Redirect user after log in and change password

--- 22 - Add Authentication to React App/src/components/Profile/ProfileForm.js
+++ 22 - Add Authentication to React App/src/components/Profile/ProfileForm.js
@@ -1,8 +1,10 @@
 import { useRef, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import AuthContext from '../../store/auth-context';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+  const history = useHistory();
   const inputPasswordRef = useRef();
   const authCtx = useContext(AuthContext);
 
@@ -22,6 +24,7 @@ const ProfileForm = () => {
         'Content-Type': 'application/json',
       },
     }).then((res) => {
+      history.replace('/');
       //assumption always success
     });
   };
