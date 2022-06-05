171cb60775943f3ca952458169310bbbf393c50a
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-06-05 13:27:02 +0700

    Use imperative navigation to show detail page

---
+++ 23A/pages/[meetupId]/index.js
@@ -0,0 +1,7 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return <MeetupDetail image="https://www.ana.co.jp/www2/plan-book/where-we-travel/paris/paris-01.jpg" title="The First Meetup" address="Some street 05, Paris" description="Some descriptions" />;
+}
+
+export default MeetupDetails;
