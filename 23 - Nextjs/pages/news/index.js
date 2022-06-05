cad05f3d84ae045ad1c6720aff2ca0115c2f048a
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-06-05 09:11:31 +0700

    Link between pages

--- 23 - Nextjs/pages/news/index.js
+++ 23 - Nextjs/pages/news/index.js
@@ -1,5 +1,18 @@
+import { Fragment } from 'react';
+import Link from 'next/link';
+
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>;
+      <ul>
+        <li>
+          <Link href="/news/next-js-is-a-great-framework">NextJS is a great framework</Link>
+        </li>
+        <li>NextJS is good for SEO</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
