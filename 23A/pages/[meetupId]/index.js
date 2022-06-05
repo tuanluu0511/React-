b08d0dfe38781248307545a705d4aa2ac1239ca5
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-06-05 15:47:31 +0700

    Work with Params and getStaticPath for SSG

--- 23A/pages/[meetupId]/index.js
+++ 23A/pages/[meetupId]/index.js
@@ -4,4 +4,41 @@ function MeetupDetails() {
   return <MeetupDetail image="https://www.ana.co.jp/www2/plan-book/where-we-travel/paris/paris-01.jpg" title="The First Meetup" address="Some street 05, Paris" description="Some descriptions" />;
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+  //print in console
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image: 'https://www.ana.co.jp/www2/plan-book/where-we-travel/paris/paris-01.jpg',
+        title: 'The First Meetup',
+        address: 'Some street 05, Paris',
+        description: 'Some description',
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
